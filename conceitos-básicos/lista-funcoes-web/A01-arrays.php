6dd80dba4da305a43e1ed1e45e4584e543e8fa74
Marcos Vinicius dos Reis Alves <75844594+Marcos1110@users.noreply.github.com>
2023-09-29 14:24:27 -0300

    Add files via upload

---
+++ conceitos-básicos/lista-funcoes-web/A01-arrays.php
@@ -0,0 +1,13 @@
+<?php
+//Aula 01 - Arrays
+
+$listaAlunos =  ["Marcos Vinícius", "Guilherme Santos", "Hiago Henrique", 
+				 "Pedro Lucas", "Pedro Paulo", "Gustavo silva"];
+
+echo "<< ALUNOS >>" . PHP_EOL;
+
+for ($indice = 0; $indice < count($listaAlunos); $indice++) {
+    echo $listaAlunos[$indice] . PHP_EOL;
+}
+
+?>
